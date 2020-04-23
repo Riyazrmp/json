53ed1b13a932a782d3b34668f953f0bc9f1d3af2
Riyazrmp <63150193+Riyazrmp@users.noreply.github.com>
2020-04-23 06:10:03 -0700

    Update get.php

--- get.php
+++ get.php
@@ -1,16 +1,7 @@
 <?php
-$dns = ‘mysql:host=localhost;dbname=bellbird_Medusa’;
-$user = ‘bellbird_Medusa’;
-$password = ‘rampur123’;
-try{
- $db = new PDO ($dns, $user, $pass);
-}catch( PDOException $e){
- $error = $e->getMessage();
- echo $error;
-}
+require_once('db.php');
 $query = 'SELECT * FROM users';
 $stm = $db->prepare($query);
 $stm->execute();
 $row = $stm->fetch(PDO::FETCH_ASSOC);
 echo json_encode($row);
-?> 
