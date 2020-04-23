72184d60c12be30bef12982e4c6d1210d585cef1
Riyazrmp <63150193+Riyazrmp@users.noreply.github.com>
2020-04-23 09:38:49 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 try{
 
-$connection = new PDO('mysql:host=localhost;dbname=bellbird_Medusa','bellbird_Medusa','rampur123')
+$connection = new PDO('mysql:host=localhost;dbname=bellbird_Medusa','bellbird_Medusa','rampur123');
 $connection -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 echo "connected";
 }
