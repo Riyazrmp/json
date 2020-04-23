2e0d8e4dda93cd8ad4b82defdc4c515fe2c7d7c3
Riyazrmp <63150193+Riyazrmp@users.noreply.github.com>
2020-04-23 07:08:23 -0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,7 +3,8 @@
 $connect = new mysqli("localhost","bellbird_Medusa","rampur123","bellbird_Medusa");
 
 if($connect){
-	 
+	 	echo "Connection Successfull";
+
 }else{
 	echo "Connection Failed";
 	exit();
