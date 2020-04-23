5a0e102de86ffaa9f6f1a08edb60d3191b80540f
Riyazrmp <63150193+Riyazrmp@users.noreply.github.com>
2020-04-23 08:15:20 -0700

    Create conn2.php

---
+++ conn2.php
@@ -0,0 +1,13 @@
+<?php
+
+$connect =new mysqli(127.0.0.2,"bellbird_Medusa","rampur123","bellbird_Medusa");
+
+if($connect){
+	 	echo "Connection Successfull";
+
+}else{
+	echo "Connection Failed";
+	exit();
+}
+
+?>
